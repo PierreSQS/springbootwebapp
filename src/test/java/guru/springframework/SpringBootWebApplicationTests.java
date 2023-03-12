01b2bb5769558ae96f70eaad9d6ce9b8ffb1ec38
PierreSQS <nomo_messina@web.de>
2023-03-12 02:53:28 +0100

    Sec4_Chap35-Sample SB Application: added assertion to test

--- src/test/java/guru/springframework/SpringBootWebApplicationTests.java
+++ src/test/java/guru/springframework/SpringBootWebApplicationTests.java
@@ -2,13 +2,17 @@ package guru.springframework;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
 class SpringBootWebApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext appCtx) {
+		assertThat(appCtx).isNotNull();
 	}
 
 }
