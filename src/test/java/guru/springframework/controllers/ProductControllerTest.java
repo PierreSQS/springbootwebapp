950cd036bf303aef15fee6e15359fa882141afbd
PierreSQS <nomo_messina@web.de>
2023-03-18 08:50:53 +0100

    Sec4_Chap35-Sample SB Application: made the test template to work

--- src/test/java/guru/springframework/controllers/ProductControllerTest.java
+++ src/test/java/guru/springframework/controllers/ProductControllerTest.java
@@ -1,9 +1,21 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.ProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+@WebMvcTest(ProductController.class)
 class ProductControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    ProductService productServMock;
 
     @BeforeEach
     void setUp() {
