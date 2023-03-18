67472b6b0828db44d60537fda753863b4b9cf507
PierreSQS <nomo_messina@web.de>
2023-03-18 12:53:43 +0100

    Sec4_Chap35-Sample SB Application-NOK: implemented the loging-test

--- src/test/java/guru/springframework/controllers/ProductControllerTest.java
+++ src/test/java/guru/springframework/controllers/ProductControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
@@ -31,8 +32,19 @@ class ProductControllerTest {
     @MockBean
     ProductService productServMock;
 
+    Product product1, product2;
+
     @BeforeEach
     void setUp() {
+        product1 = new Product();
+        product1.setProductId(String.valueOf(1));
+        product1.setPrice(BigDecimal.valueOf(11L));
+        product1.setDescription("Test Product1");
+
+        product2 = new Product();
+        product2.setProductId(String.valueOf(2));
+        product2.setPrice(BigDecimal.valueOf(22L));
+        product2.setDescription("Test Product2");
     }
 
     @Test
@@ -43,13 +55,15 @@ class ProductControllerTest {
     @WithMockUser(username = "MockUser")
     void list() throws Exception {
         // Given
-        given(productServMock.listAllProducts()).willReturn(List.of(new Product(),new Product()));
+        given(productServMock.listAllProducts()).willReturn(List.of(product1,product2));
 
         // When, Then
         mockMvc.perform(get("/products"))
                 .andExpect(status().isOk())
                 .andExpect(model().attributeExists("products"))
                 .andExpect(content().string(containsString("<span>Welcome <span>MockUser</span></span>")))
+                .andExpect(content().string(containsString("<td>Test Product1</td>")))
+                .andExpect(content().string(containsString("<td>Test Product2</td>")))
                 .andExpect(view().name("products"))
                 .andDo(print());
     }
@@ -90,7 +104,17 @@ class ProductControllerTest {
         verify(productServMock).deleteProduct(any());
     }
 
+    /**
+     * This is an example which shows that we are using the Default Config
+     * the test fails because the request is redirected to the Default login Page
+     * thus the test fails
+     */
     @Test
-    void login() {
+    @WithMockUser(username = "MockUser")
+    void login() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"))
+                .andDo(print());
     }
 }
