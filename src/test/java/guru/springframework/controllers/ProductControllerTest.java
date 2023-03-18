4f11a79a4ef4eb06b2045e76d33860eee8d7a7d1
PierreSQS <nomo_messina@web.de>
2023-03-18 08:34:29 +0100

    Sec4_Chap35-Sample SB Application: added an empty test template

---
+++ src/test/java/guru/springframework/controllers/ProductControllerTest.java
@@ -0,0 +1,43 @@
+package guru.springframework.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ProductControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setProductService() {
+    }
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void showProduct() {
+    }
+
+    @Test
+    void edit() {
+    }
+
+    @Test
+    void newProduct() {
+    }
+
+    @Test
+    void saveProduct() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void login() {
+    }
+}
