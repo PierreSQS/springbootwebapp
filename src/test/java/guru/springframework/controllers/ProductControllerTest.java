20406f4cdabbe48c0ceb0d394408e79c08c9cd46
PierreSQS <nomo_messina@web.de>
2023-03-18 09:17:46 +0100

    Sec4_Chap35-Sample SB Application: implemented list()-Test

--- src/test/java/guru/springframework/controllers/ProductControllerTest.java
+++ src/test/java/guru/springframework/controllers/ProductControllerTest.java
@@ -1,12 +1,22 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Product;
 import guru.springframework.services.ProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ProductController.class)
 class ProductControllerTest {
@@ -26,7 +36,18 @@ class ProductControllerTest {
     }
 
     @Test
-    void list() {
+    @WithMockUser(username = "MockUser")
+    void list() throws Exception {
+        // Given
+        given(productServMock.listAllProducts()).willReturn(List.of(new Product(),new Product()));
+
+        // When, Then
+        mockMvc.perform(get("/products"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("products"))
+                .andExpect(content().string(containsString("<span>Welcome <span>MockUser</span></span>")))
+                .andExpect(view().name("products"))
+                .andDo(print());
     }
 
     @Test
