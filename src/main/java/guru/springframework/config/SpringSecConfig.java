6c8725c7f9a3f08cb0a67acabcd044ed41f48249
PierreSQS <nomo_messina@web.de>
2023-03-27 22:22:15 +0200

    Sec4_Chap35-Sample SB Application: removed the redundant Authentication Provider

--- src/main/java/guru/springframework/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/config/SpringSecConfig.java
@@ -3,10 +3,7 @@ package guru.springframework.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,14 +14,6 @@ public class SpringSecConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean("daoAuthenticationProvider")
-    public AuthenticationProvider daoAuthenticationProvider(UserDetailsService userDetailsService) {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder());
-        provider.setUserDetailsService(userDetailsService);
-        return provider;
     }
 
     @Bean
