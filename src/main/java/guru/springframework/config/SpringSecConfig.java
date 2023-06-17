8d5535bf88ba6e12387cdf72fda6603bcc61e62d
PierreSQS <nomo_messina@web.de>
2023-06-17 13:33:40 +0200

    Sec4_Chap35-Sample SB Application: upgraded SB to 3.1.0, Maven to 3.9.2 and upgrade Sec. Config.

--- src/main/java/guru/springframework/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/config/SpringSecConfig.java
@@ -4,6 +4,8 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,18 +21,18 @@ public class SpringSecConfig {
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
            // ignore CSRF for H2-Console
-           httpSecurity.csrf().ignoringRequestMatchers(PathRequest.toH2Console())
-                .and()
-                .authorizeHttpRequests()
-                   .requestMatchers(PathRequest.toH2Console()).permitAll()
-                   .requestMatchers("/","/products","/product/show/*").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                    .formLogin().loginPage("/login").permitAll()
-                .and()
-                    .logout().permitAll();
+           httpSecurity
+                   .csrf(csrfConfigurer -> csrfConfigurer.ignoringRequestMatchers(PathRequest.toH2Console()))
+                   .authorizeHttpRequests(authorize ->
+                           authorize.requestMatchers(PathRequest.toH2Console()).permitAll()
+                                    .requestMatchers("/","/products","/product/show/*").permitAll()
+                                   .anyRequest().authenticated()
+                   )
+                   .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login").permitAll())
+                   .logout(LogoutConfigurer::permitAll);
 
-        httpSecurity.headers().frameOptions().sameOrigin();
+           httpSecurity.headers(httpHeadersConfigurer ->
+                httpHeadersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
 
         return httpSecurity.build();
     }
